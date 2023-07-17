dec556eb788205cb5b45c4eea8122e96b3b2eb8a
dreamNawaporn <96407096+dreamNawaporn@users.noreply.github.com>
2023-07-17 11:13:49 +0700

    Initial commit

---
+++ MathTestDriver.java
@@ -0,0 +1,9 @@
+
+public class MathTestDriver {
+  public static void main(String[] args) {
+      MathSingleton m1 =  MathSingleton.getInstance();
+      System.out.println("316 + 13 = "+m1.add(316, 13) );
+ 	  System.out.println("316 + 13 = "+m1.add(316, 13));
+      
+  }
+}
---
+++ src/MathTesDriver.java
@@ -0,0 +1,8 @@
+
+public class MathTesDriver {
+  public static void main(String[] args) {
+      MathSingleton m1 =  MathSingleton.getInstance();
+      System.out.println("316 + 13 = "+m1.add(316, 13));
+      
+  }
+}
